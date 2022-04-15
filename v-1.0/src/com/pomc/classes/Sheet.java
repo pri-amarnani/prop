971f195defb81510a016180ae034b2337993211f
cristina.bretons <cristina.bretons@estudiantat.upc.edu>
2022-04-15 11:41:16 +0200

    From Date to LocalDate. Row/Columns functions.

--- v-1.0/src/com/pomc/classes/Sheet.java
+++ v-1.0/src/com/pomc/classes/Sheet.java
@@ -1,88 +1,111 @@
 package com.pomc.classes;
 
-//import java.time.LocalDate;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.Vector;
 
 public class Sheet {
-    Vector<Vector<Cell>> celdas;
+    Vector<Vector<Cell>> cells;
     String title;
     int num_rows;
     int num_cols;
+    Block b_selected;
 
     public Sheet(String title) {
-        this.title = title;
-        celdas = null;
+        if(title != null) this.title = title;
+        else this.title = "titulo por defecto";
+        cells = null;
         num_cols = 0;
         num_rows = 0;
     }
 
-    public Sheet(Vector<Vector<Cell>> celdas, String title) {  //not sure
-        this.title = title;
-        this.celdas = celdas;
-        num_rows = celdas.size();
-        num_cols = celdas.elementAt(0).size();
+    public Sheet(Vector<Vector<Cell>> cells, String title) {  //not sure
+        if(title != null) this.title = title;
+        else this.title = "titulo por defecto";
+        this.cells = cells;
+        num_rows = cells.size();
+        num_cols = cells.elementAt(0).size();
     }
 
     public Sheet(int rows, int columns, String title){
         this.num_rows = rows;
         this.num_cols = columns;
         this.title = title;
-        cells(rows, columns);
-    }
-
-    public void cells(int rows, int cols){ //las celdas por defecto son numericas
-        title = "titulo por defecto"; //poner titulo por defecto
         String n = "N";
         for(int i = 0; i < rows; ++i){
-            celdas.add(new Vector<Cell>(rows));
-            for(int j = 0; j < cols; ++j){
-                //celdas.elementAt(i).elementAt(j) = new Cell(i, j, n);     //creadora de cell?
-                //-> Cell' is abstract; cannot be instantiated
+            Vector<Cell> row = new Vector<>();
+            for(int j = 0; j < columns; ++j){
+                row.add(new Cell(i, j, n));
             }
+            cells.add(row);
         }
     }
 
-    /*
-    public void NewRow(int pos){  //docSheets.removeElement(i);
+    public void NewRow(int pos){
         ++num_rows;
-        if (pos != celdas.size()) celdas.add(new ArrayList<Cell>()); //celdas.add(celdas[celdas.size()-1]);
-        for(int i = celdas.size()-2; i >= pos; --i){      //necesito funcion para cambiar el tipo
-            celdas[i+1] = celdas[i];
+        Vector<Cell> pos_row = new Vector<Cell>();
+        for(int i = 0; i < num_cols; ++i){
+            Cell c = new Cell(pos, i,"N");
+            pos_row.add(c);
         }
-        celdas[pos] =new  Cell[][]; //vaciar la info
+        cells.insertElementAt(pos_row, pos);
+        for(int i = pos + 1; i < num_rows; ++i){
+            Vector<Cell> row = cells.elementAt(i);
+            for(int j = 0; j < num_cols; ++j){
+                Cell c = row.elementAt(j);
+                c.setRow(i);
+                row.setElementAt(c,j);
+            }
+            cells.setElementAt(row,i);
+        }
     }
 
     public void NewColumn(int pos){
         ++num_cols;
-        //NO MOVER LAS CELDAS, SOLO LA INFO QUE TIENEN!!!
-        for(int i = 0; i < celdas.size(); ++i){
-            for(int j = celdas[0].size(); j >= pos; --j ) {
-                if(j == celdas[0].size()) celdas[i].add(celdas[i][j-1]);
-                else if(j == pos) celdas[i][j] = new Cell;  //vaciar la info
-                else celdas[i][j] = celdas[i][j-1];
+        for(int i = 0; i < num_rows; ++i){
+            Vector<Cell> row = cells.elementAt(i);
+            row.insertElementAt(new Cell(i,pos,"N"), pos);
+            for(int j = pos + 1; j < num_cols; ++j ) {
+                Cell c = row.elementAt(j);
+                c.setColumn(j);
+                row.setElementAt(c,j);
             }
+            cells.setElementAt(row,i);
         }
     }
 
     public void DeleteRow(int pos){
-        --num_rows; //salta si solo hay una fila
-        for(int i = pos; i < celdas.size()-1; ++i){      //necesito funcion para cambiar el tipo
-            celdas[i] = celdas[i+1];
-        }
-        celdas.remove(celdas[celdas.size()-1]); //pop?
-    }
-    public void DeleteColumn(int pos){
-        --num_cols; //salta si solo hay una columna
-        for(int i = 0; i < celdas.size(); ++i){
-            for(int j = pos; j < celdas[0].size()-1; ++j ) {
-                celdas[i][j] = celdas[i][j+1];
+        if(num_rows <= 0) System.out.println("Error. Not enough rows.");
+        else{
+            --num_rows;
+            cells.removeElement(pos);
+            for(int i = pos; i < num_rows; ++i){
+                Vector<Cell> row = cells.elementAt(i);
+                for(int j = 0; j < num_cols; ++j){
+                    Cell c = row.elementAt(j);
+                    c.setRow(i);
+                    row.setElementAt(c,j);
+                }
+                cells.setElementAt(row,i);
             }
-            celdas[i].remove(celdas[i][celdas[0].size()-1]);
         }
     }
 
-*/
+    public void DeleteColumn(int pos){
+        if(num_cols <= 0) System.out.println("Error. Not enough columns.");
+        else{
+            --num_cols;
+            for(int i = 0; i < num_rows; ++i){
+                Vector<Cell> row = cells.elementAt(i);
+                row.removeElementAt(pos);
+                for(int j = pos; j < num_cols; ++j ) {
+                    Cell c = row.elementAt(j);
+                    c.setColumn(j);
+                    row.setElementAt(c,j);
+                }
+                cells.setElementAt(row,i);
+            }
+        }
+    }
 
     public Block SelectBloc(Cell c1, Cell c2){ //1 bloque seleccionado + crear otro en las aritmeticas
         //crear bloque y devolverlo
@@ -106,7 +129,7 @@ public class Sheet {
         b.ModifyBlock(s);
     }
 
-    public void ModifyBlock(Block b, Date ld){
+    public void ModifyBlock(Block b, LocalDate ld){
         b.ModifyBlock(ld);
     }
 
@@ -127,7 +150,7 @@ public class Sheet {
     public Cell find(String s, Block b){
         return b.find(s);
     }
-    public Cell find(Date ld, Block b){
+    public Cell find(LocalDate ld, Block b){
         return b.find(ld);
     }
 
@@ -135,10 +158,10 @@ public class Sheet {
     public void findAndReplace(double n, double r, Block b){
         b.findAndReplace(n); //(n,r)
     }
-    public void findAndReplace(String s, Block b){
+    public void findAndReplace(String s, Block b){ //pasar dos doubles
         b.findAndReplace(s);
     }
-    public void findAndReplace(Date ld, Block b){
+    public void findAndReplace(LocalDate ld, Block b){ //pasar dos doubles
         b.findAndReplace(ld);
     }
 
@@ -148,11 +171,11 @@ public class Sheet {
         b1.floor(b2, ref);
     }
 
-    public void convert(Block b1, Cell c, Boolean ref){
+    public void convert(Block b1, Cell c, Boolean ref){ //falta acabar
         b1.convert(b1, ref);
     }
 
-    public void sum(Block b1, Block b2, Block b3, Boolean ref){  //lo guardamos en cell?
+    public void sum(Block b1, Block b2, Block b3, Boolean ref){
         b1.sum(b2, b3, ref);
     }
 
@@ -168,11 +191,11 @@ public class Sheet {
         b1.substract(b2, b3, ref);
     }
 
-    public void extract(Block b1,Cell c, Boolean ref){
+    public void extract(Block b1,Cell c, Boolean ref){ //falta acabar
         b1.extract(b1, ref);
     }
 
-    public void dayOfTheWeek (Block b1,Cell c, Boolean ref){
+    public void dayOfTheWeek (Block b1,Cell c, Boolean ref){ //q hace exactamente?
         b1.dayOfTheWeek(b1, ref);
     }
 
@@ -185,52 +208,34 @@ public class Sheet {
     }
 
     public void mean(Block b1,Cell c, Boolean ref){
-        b1.mean(ref);
+        c.setType("N");
+        c.changeValueN(b1.mean(ref));
     }
 
     public void median(Block b1, Cell c, Boolean ref){
-        b1.median(ref);
+        c.setType("N");
+        c.changeValueN(b1.median(ref));
     }
 
     public void var(Block b1,Cell c, Boolean ref){
-        if(c.isNum()) c.changeValueN(b1.var(ref));
-        else if(c.isText()){
-            //cambiar el tipo de la celda a numerica
-            //c.changeValueT(b1.var(ref));
-        }
-        else if(c.isDate()){
-            //cambiar el tipo de la celda a numerica
-            //c.changeValueD(b1.var(ref));
-        }
+        c.setType("N");
+        c.changeValueN(b1.var(ref));
     }
 
     public void covar(Block b1, Block b2, Cell c, Boolean ref){
-        if(c.isNum()) c.changeValueN(b1.covar(b1,ref));
-        else if(c.isText()){
-            //cambiar el tipo de la celda a numerica
-            //c.changeValueT(b1.covar(b1,ref));    //siempre devuelve un float!!
-        }
-        else if(c.isDate()){
-            //cambiar el tipo de la celda a numerica
-            //c.changeValueD(b1.covar(b1,ref));
-        }
+        c.setType("N");
+        c.changeValueN(b1.covar(b1,ref));
         //else (); //control de errores
     }
 
-    public void std(Block b1,Cell c, Boolean ref){  //mirar en todas de que tipo es el bloque (numerico)
-        b1.std(ref);
+    public void std(Block b1,Cell c, Boolean ref){
+        c.setType("N");
+        c.changeValueN(b1.std(ref));
     }
 
     public void CPearson(Block b1, Cell c, Boolean ref){ //crear bloque desde la celda c
         Block b2 = new Block();
-        if(c.isNum()) c.changeValueN(b1.CPearson(b2, ref));
-        else if(c.isText()){
-            //cambiar el tipo de la celda a numerica
-            //c.changeValueN(b1.CPearson(b2, ref));
-        }
-        else if(c.isDate()){
-            //cambiar el tipo de la celda a numerica
-            //c.changeValueN(b1.CPearson(b2, ref));
-        }
+        c.setType("N");
+        c.changeValueN(b1.CPearson(b2, ref));
     }
 }
