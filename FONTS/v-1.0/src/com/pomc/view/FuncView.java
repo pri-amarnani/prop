ab407e05a276b908ada2a91e94a979bac37104bf
omar.tanveer.butt <omar.tanveer.butt@estudiantat.upc.edu>
2022-05-30 22:31:16 +0200

    UI 😈😈

--- FONTS/v-1.0/src/com/pomc/view/FuncView.java
+++ FONTS/v-1.0/src/com/pomc/view/FuncView.java
@@ -702,7 +702,7 @@ public class FuncView {
         int result = JOptionPane.showConfirmDialog(
                 null,
                 fields,
-                "Move block",
+                "Copy block",
                 JOptionPane.OK_CANCEL_OPTION,
                 JOptionPane.PLAIN_MESSAGE,
                 null
@@ -740,7 +740,7 @@ public class FuncView {
             int result2 = JOptionPane.showConfirmDialog(
                     null,
                     fields2,
-                    "Move block to: ",
+                    "Copy block to: ",
                     JOptionPane.OK_CANCEL_OPTION,
                     JOptionPane.PLAIN_MESSAGE,
                     null
